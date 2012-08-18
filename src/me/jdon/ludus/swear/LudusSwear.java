81f8725218d05ae99f90bc230ee69fb0e647d246
Jonathan Donaldson <djd206@gmail.com>
2012-08-18 08:39:10 +0100

    Updated to V1.4

--- src/me/jdon/ludus/swear/LudusSwear.java
+++ src/me/jdon/ludus/swear/LudusSwear.java
@@ -12,7 +12,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -44,7 +45,7 @@ public class LudusSwear extends JavaPlugin implements Listener{
 	
 	// lowest event priority to stop lagg
 	 @EventHandler(priority = EventPriority.LOWEST)
-	 public void chat(final PlayerChatEvent ev) {
+	 public void chat(final AsyncPlayerChatEvent  ev) {
 		 if(ev.isCancelled())return;
 		 if(ev.getPlayer().hasPermission("sj.exempt"))return;
 		 List<String> swears = getConfig().getStringList("swears");
@@ -143,16 +144,37 @@ public class LudusSwear extends JavaPlugin implements Listener{
     }
    }
 }
-	 public String swearjar(Player p, String m){
-		Double amount = this.getConfig().getDouble("Fine");
+	 public String swearjar(final Player p, final String m){
+		final Double amount = this.getConfig().getDouble("Fine");
 			final double bal = economy.getBalance(p.getName());
 			if (bal < amount) {
-				p.sendMessage(tag+"You cant afford to say '"+ m+"'.");
-				return "poor";
+					this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+						   public void run() {
+								p.sendMessage(tag+"You cant afford to say '"+ m+"'.");
+						   }
+						}, 1L);
+					return "poor";
 			} else {
+				if(this.getConfig().getBoolean("DisableSwearing")){
+					economy.withdrawPlayer(p.getName(), amount);
+					this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+						   public void run() {
+							   p.sendMessage(tag+"You have been charged "+amount+" "+economy.currencyNamePlural()+ " for saying '"+m+"'.");
+						   }
+						}, 1L);
+					return "poor";
+				}else{
 				economy.withdrawPlayer(p.getName(), amount);
-				p.sendMessage(tag+"You have been charged "+amount+" "+economy.currencyNamePlural()+ " for saying '"+m+"'.");
+				this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+					   public void run() {
+						   p.sendMessage(tag+"You have been charged "+amount+" "+economy.currencyNamePlural()+ " for saying '"+m+"'.");
+					   }
+					}, 1L);
 				return "rich";
+				}
 			}
 	 }
 	  // Setup Economy (Vault)
@@ -343,6 +365,107 @@ public class LudusSwear extends JavaPlugin implements Listener{
 		    if ( s.length() <= 1 ) return s;
 		    if( s.substring(1,2).equals(s.substring(0,1)) ) return removeDups(s.substring(1));
 		    else return s.substring(0,1) + removeDups(s.substring(1));
+		}
+		@EventHandler
+		public void commands(PlayerCommandPreprocessEvent  ev){
+			if(this.getConfig().getBoolean("commandChecking") == false)return;
+			if(ev.isCancelled())return;
+			 if(ev.getPlayer().hasPermission("sj.exempt"))return;
+			 List<String> swears = getConfig().getStringList("swears");
+			 List<String> exception = getConfig().getStringList("exception");
+			 String message = ev.getMessage().toLowerCase();
+	    	 String mess = message.replace("*", "");
+	    	 String mes3 = mess.replace("()", "o");
+	    	 String mes2 = mes3.replace("(", "");
+	    	 String mes = mes2.replace(")", "");
+	    	 String me1 = mes.replace("/", "");
+	    	 String me2 = me1.replace(".", "");
+	    	 String me3 = me2.replace(",", "");
+	    	 String me4 = me3.replace("4", "a");
+	    	 String me5 = me4.replace(";", "");
+	    	 String me6 = me5.replace("'", "");
+	    	 String me7 = me6.replace("#", "");
+	    	 String me8 = me7.replace("~", "");
+	    	 String me9 = me8.replace("^", "");
+	    	 String me10 = me9.replace("-", "");
+	    	 String me11 = me10.replace("+", "");
+	    	 String me12 = me11.replace("1", "i");
+	    	 String me13 = me12.replace("0", "o");
+	    	 String me14 = me13.replace("@", "o");
+	    	 String me15 = me13.replace("@", "a");
+	    	 String final1 = removeDups(me14);
+	    	 String final2 = removeDups(me15);
+	    	 String final1space = final1.replaceAll(" ", "");
+			 String me15space = me15.replaceAll(" ", "");
+			 String final2space = final2.replaceAll(" ", "");
+			 String me14space = me14.replaceAll(" ", "");
+	         for (String s : swears){
+	        	 for (String e : exception){
+	            	 if(final1.contains(e))return;
+	            	 if(me15.contains(e))return;
+	            	 if(me14.contains(e))return;
+	            	 if(final2.contains(e))return;
+	             }
+	        	 if(this.getConfig().getBoolean("aggressive")){
+	        	 if(final1space.contains(s)){
+	        		 if(swearjar(ev.getPlayer(), s) == "poor"){
+	        			 ev.setCancelled(true);
+	        		 }
+	        	 }
+	        	 // leave duplicatios and remove numbers and special characters
+	        	 if(!(final1space.contains(s))){
+	        		if(me15space.contains(s)){
+	        			 if(swearjar(ev.getPlayer(), s) == "poor"){
+	            			 ev.setCancelled(true);
+	        			 }
+	        		}
+	        	 }
+	        		// leave dulipcates
+	        	 if(!(final1space.contains(s) || me15space.contains(s))){
+	        		if(me14space.contains(s)){
+	       			 if(swearjar(ev.getPlayer(), s) == "poor"){
+	        			 ev.setCancelled(true);
+	    			 }
+	          }
+	        	 }
+	        	 if(!(final1space.contains(s) || me15space.contains(s) || me14space.contains(s))){
+	        		if(final2space.contains(s)){
+	          			 if(swearjar(ev.getPlayer(), s) == "poor"){
+	           			 ev.setCancelled(true);
+	       			 }
+	     }
+	   }
+	    }else{
+		 if(final1.contains(s)){
+			 if(swearjar(ev.getPlayer(), s) == "poor"){
+				 ev.setCancelled(true);
+			 }
+		 }
+		 // leave duplicatios and remove numbers and special characters
+		 if(!(final1.contains(s))){
+			if(me15.contains(s)){
+				 if(swearjar(ev.getPlayer(), s) == "poor"){
+	    			 ev.setCancelled(true);
+				 }
+			}
+		 }
+			// leave dulipcates
+		 if(!(final1.contains(s) || me15.contains(s))){
+			if(me14.contains(s)){
+				 if(swearjar(ev.getPlayer(), s) == "poor"){
+				 ev.setCancelled(true);
+			 }
+	  }
+		 }
+		 if(!(final1.contains(s) || me15.contains(s) || me14.contains(s))){
+			if(final2.contains(s)){
+	  			 if(swearjar(ev.getPlayer(), s) == "poor"){
+	   			 ev.setCancelled(true);
+				 }
+	}
+	}
+	    }
+	   }
 		}
 	
 }
